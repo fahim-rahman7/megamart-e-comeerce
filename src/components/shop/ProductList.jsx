1ab8ee476c096d6bad3af4ce750d74c8885cb99a
Fahim Rahman <fahimrahmandipto@gmail.com>
2026-08-11 04:38:08 +0600

    Loading component fixed 2

--- src/components/shop/ProductList.jsx
+++ src/components/shop/ProductList.jsx
@@ -75,7 +75,7 @@ const ProductList = () => {
 
             {/* Products Grid */}
             {isLoading || isFetching ? (
-              <Loading count={limit}/>
+              <Loading count={limit} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5"/>
             ) : (
               <>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5">
